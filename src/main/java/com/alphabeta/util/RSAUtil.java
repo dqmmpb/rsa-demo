4a7cafe40733b692ee7bd215f0daaeb214fa5b03
邓启明 <dengqiming@xiaomadada.com>
2016-12-24 15:46:46 +0800

    Init

---
+++ src/main/java/com/alphabeta/util/RSAUtil.java
@@ -0,0 +1,109 @@
+package com.alphabeta.util;
+
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.Provider;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.util.encoders.Hex;
+
+/**
+ * ==================================================================
+ * RSAUtil.java created by alphabeta
+ *
+ * @author alphabeta
+ * @version 1.0
+ *          ==================================================================
+ */
+
+public class RSAUtil {
+
+  public static final Provider pro = new BouncyCastleProvider();
+
+  private static final String charSet = "UTF-8";
+
+  // 种子,改变后,生成的密钥对会发生变化
+  private static final String seedKey = "seedKey";
+  
+  public static KeyPair generateKeyPair() throws Exception {
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", pro);
+    kpg.initialize(1024, new SecureRandom(seedKey.getBytes()));
+    KeyPair kp = kpg.generateKeyPair();
+
+    return kp;
+  }
+
+/*  public static PublicKey getPublicRSAKey(String modulus, String exponent) throws Exception {
+    RSAPublicKeySpec spec = new RSAPublicKeySpec(new BigInteger(modulus, 16), new BigInteger(exponent, 16));
+    KeyFactory kf = KeyFactory.getInstance("RSA", pro);
+    return kf.generatePublic(spec);
+  }
+*/
+  public static PublicKey getPublicRSAKey(String key) throws Exception {
+    X509EncodedKeySpec x509 = new X509EncodedKeySpec(Base64.decode(key));
+    KeyFactory kf = KeyFactory.getInstance("RSA", pro);
+    return kf.generatePublic(x509);
+  }
+
+  public static PrivateKey getPrivateRSAKey(String key) throws Exception {
+    PKCS8EncodedKeySpec pkgs8 = new PKCS8EncodedKeySpec(Base64.decode(key));
+    KeyFactory kf = KeyFactory.getInstance("RSA", pro);
+    return kf.generatePrivate(pkgs8);
+  }
+
+  public static byte[] encrypt(String input, PublicKey publicKey) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA/None/PKCS1Padding", pro);
+    cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+    byte[] re = cipher.doFinal(input.getBytes(charSet));
+    return re;
+  }
+
+  public static byte[] decrypt(byte[] encrypted, PrivateKey privateKey) throws Exception {
+    Cipher cipher = Cipher.getInstance("RSA/None/PKCS1Padding", pro);
+    cipher.init(Cipher.DECRYPT_MODE, privateKey);
+    byte[] re = cipher.doFinal(encrypted);
+    return re;
+  }
+
+  public static void main(String[] args) throws Exception {
+    // KeyPair kp = generateKeyPair();
+    //
+    // PrivateKey privateKey = kp.getPrivate();
+    // byte[] prk = privateKey.getEncoded();
+    // String privateKeyStr = new String(Base64.encode(prk));
+    //
+    // PublicKey publicKey = kp.getPublic();
+    // byte[] pbk = publicKey.getEncoded();
+    // String publicKeyStr = new String(Base64.encode(pbk));
+    //
+    // System.out.println("PrivateKey: " + privateKey);
+    // System.out.println("PublicKey: " + publicKey);
+    //
+    // System.out.println("pstr:" + privateKeyStr);
+    // System.out.println("publicstr:" + publicKeyStr);
+
+    // String modulus =
+    // ((BCRSAPublicKey)publicKey).getModulus().toString(16);
+    // String exponent =
+    // ((BCRSAPublicKey)publicKey).getPublicExponent().toString(16);
+    // modules:
+    // a0a36434e33aa15c1ef1335dc2268054323d56411e1e2b13c3986e9cb5b800efed39cd812dd6a1ecc522113084539e30649b83db7b4ef5a510f3081494e3fb5c6c17b09d8e0c49f671f211e09721d1443bfdb2b45654f33138b22b80e716c6494128e8d149ebc028c1658dfdbc86bb049b4d7faab7260c75d82670cc4cc2ffd9
+    // System.out.println("modulus: " + modulus);
+    // System.out.println("exponent: " + exponent);
+    // PublicKey pb = getPublicRSAKey(modulus, exponent);
+    // System.out.println("PublicKey2: " + pb);
+    
+  }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,105 @@
+<%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" %>
+
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+  <title>rsa</title>
+
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  <link type="image/x-icon" rel="shortcut icon" href="favicon.ico">
+  
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    .panel {
+      background: #ddd;
+      padding: 10px;
+      margin: 10px;
+    }
+    .title {
+      font-weight: bold;
+      font-size: 16px;
+    }
+    label {
+      color: red;
+    }
+    .value {
+      word-break: break-word;
+    }
+    .warning {
+      color: white;
+      font-size: 80%;
+      background: orange;
+    }
+  </style>
+ 
+</head>
+<body>
+ 
+  <div class="page-group">
+  
+   <div class="panel">
+     <div>
+       <button id="generate" type="button">生成密钥</button>
+       
+     </div>
+     <div class="warning">生成的新密钥会将"js加密，js解密"模块中的文本重新加密解密，其他模块中的不会改变</div>
+     <label>公钥</label>
+     <div id="publicKey" class="value"></div>
+     <label>私钥</label>
+     <div id="privateKey" class="value"></div>
+   </div>
+  
+   <div id="encryptJs_decryptJs" class="panel">
+     <div class="title">js加密，js解密</div>
+     <label>加密明文</label>
+     <div class="value plainText"></div>
+     <label>Js加密后</label>
+     <div class="value afterEncrypt"></div>
+     <label>Js解密后</label>
+     <div class="value afterDecrypt"></div>
+   </div>
+   
+   <div id="encryptJava_decryptJs" class="panel">
+    <div class="title">java加密，js解密</div>
+    <label>加密明文</label>
+    <div class="value plainText"></div>
+    <label>Js加密后</label>
+    <div class="value afterEncrypt"></div>
+    <label>Js解密后</label>
+    <div class="value afterDecrypt"></div>
+  </div>
+  
+  <div id="encryptJs_decryptJs2" class="panel">
+    <div class="title">js加密，js解密</div>
+    <label>加密明文</label>
+    <div class="value plainText"></div>
+    <label>Js加密后</label>
+    <div class="value afterEncrypt"></div>
+    <label>Js解密后</label>
+    <div class="value afterDecrypt"></div>
+  </div>
+  
+  
+  </div>
+
+  <script src="lib/modernizr/modernizr.js"></script>
+
+  <script src="lib/jquery/dist/jquery.js"></script>
+
+  <script src="lib/jsencrypt/bin/jsencrypt.js"></script>
+
+  <script src="js/rsa-demo.js"></script>
+
+</body>
+</html>
