f727c8df97c039c6c9069a64349c16807db5ea66
dengqiming <dengqiming@ddy98.com>
2018-03-09 10:25:31 +0800

    优化工具类

--- src/test/java/com/alphabeta/util/RSAUtilTest.java
+++ src/test/java/com/alphabeta/util/RSAUtilTest.java
@@ -28,73 +28,71 @@ public class RSAUtilTest {
 
     }
 
-//    /**
-//     * java加密， java解密
-//     */
-//    @Test
-//    public void testEncryptJavaAndDecryptJava() {
-//        try {
-//            String plainText = "Java中文";
-//            System.out.println("明文：" + plainText);
-//            String encryptedText = RSAUtil.encryptToString(plainText, publicKey);
-//            System.out.println("Java加密后：" + encryptedText);
-//            String decryptedText = RSAUtil.decryptToString(encryptedText, privateKey);
-//            System.out.println("Java解密后：" + decryptedText);
-//            Assert.assertEquals(plainText, decryptedText);
-//        } catch (Exception e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//    }
+    /**
+     * java加密， java解密
+     */
+    @Test
+    public void testEncryptJavaAndDecryptJava() {
+        try {
+            String plainText = "Java中文";
+            System.out.println("明文：" + plainText);
+            String encryptedText = RSAUtil.encryptToString(plainText, publicKey);
+            System.out.println("Java加密后：" + encryptedText);
+            String decryptedText = RSAUtil.decryptToString(encryptedText, privateKey);
+            System.out.println("Java解密后：" + decryptedText);
+            Assert.assertEquals(plainText, decryptedText);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-//    @Test
-//    public void testGenerate() {
-//        try {
-//            KeyPair kp = RSAUtil.generateKeyPair();
-//            PrivateKey privateKey = kp.getPrivate();
-//            byte[] prk = privateKey.getEncoded();
-//            String privateKeyStr = new String(Base64.encode(prk));
-//
-//            PublicKey publicKey = kp.getPublic();
-//            byte[] pbk = publicKey.getEncoded();
-//            String publicKeyStr = new String(Base64.encode(pbk));
-//
-//            System.out.println(privateKeyStr);
-//            System.out.println(publicKeyStr);
-//        } catch (Exception e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//    }
+    }
+
+    @Test
+    public void testGenerate() {
+        try {
+            KeyPair kp = RSAUtil.generateKeyPair();
+            PrivateKey privateKey = kp.getPrivate();
+            PublicKey publicKey = kp.getPublic();
+
+            String privateKeyStr = RSAUtil.toString(privateKey);
+            String publicKeyStr = RSAUtil.toString(publicKey);
+
+            System.out.println(privateKeyStr);
+            System.out.println(publicKeyStr);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 
-//    /**
-//     * 生成Pem格式的Key
-//     */
-//    @Test
-//    public void testGenerateKeyPem() {
-//        try {
-//            System.out.println(privateKey.getFormat());
-//            System.out.println(privateKey.getAlgorithm());
-//
-//            System.out.println(publicKey.getFormat());
-//            System.out.println(publicKey.getAlgorithm());
-//
-//            String privateKeyPem = RSAUtil.generatePublicRSAKeyPem(privateKey);
-//
-//            System.out.println(privateKeyPem);
-//
-//            String publicKeyPem = RSAUtil.generatePublicRSAKeyPem(publicKey);
-//
-//            System.out.println(publicKeyPem);
-//
-//        } catch (Exception e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//    }
+    /**
+     * 生成Pem格式的Key
+     */
+    @Test
+    public void testGenerateKeyPem() {
+        try {
+            System.out.println(privateKey.getFormat());
+            System.out.println(privateKey.getAlgorithm());
+
+            System.out.println(publicKey.getFormat());
+            System.out.println(publicKey.getAlgorithm());
+
+            String privateKeyPem = RSAUtil.generatePublicRSAKeyPem(privateKey);
+
+            System.out.println(privateKeyPem);
+
+            String publicKeyPem = RSAUtil.generatePublicRSAKeyPem(publicKey);
+
+            System.out.println(publicKeyPem);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 
     /**
      * 从Pem中读取Key
